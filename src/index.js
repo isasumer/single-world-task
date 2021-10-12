9ffc3da82b8cf01882bf3cddfff05d4253b8ed1f
Isa SUMER <sumerisa5@gmail.com>
2021-10-12 22:50:39 +0300

    index added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import store from "./store/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+serviceWorker.unregister();
