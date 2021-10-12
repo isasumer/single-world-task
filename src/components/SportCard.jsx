355f0cfc75af4523203d24883d17f418d58c8b71
Isa SUMER <sumerisa5@gmail.com>
2021-10-12 22:50:21 +0300

    unnecessary files deleted

--- src/components/SportCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const listCard = [];
-const SportCard = React.memo(({ info }) => {
-  listCard.push(info.value);
-  console.log(listCard);
-  return <div></div>;
-});
-
-export default SportCard;
